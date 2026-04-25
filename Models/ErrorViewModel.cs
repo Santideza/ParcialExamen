5e8b89211edaf81884ebd7a3dc2bcec7bfa5db38
Santiago Deza <santi_pro14@hotmail.com>
2026-04-25 11:27:16 -0500

    inicio main

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ParcialExamen.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
